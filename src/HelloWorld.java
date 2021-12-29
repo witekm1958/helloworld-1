236ee85f63ef08062cd50329798ac90dbb79c43a
Witold Matusiński <matusinski@interia.pl>
2021-12-30 00:33:00 +0100

    Dodałem pętlę wyświetlającą napis "Hello World" 10 razy oraz numer kolejny wyświetlenia.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println((i + 1) + " Hello World!");
+        }
     }
 }
